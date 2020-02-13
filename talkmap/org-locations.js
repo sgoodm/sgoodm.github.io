e6b9ddee2a8fb7cb19b82863a51ce12e27cc9f49
sgoodm <sgoodman@aiddata.wm.edu>
2020-02-13 14:36:15 -0500

    Remove unused pages. Update locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,46 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "AidEx. Brussels, Belgium. 2019.",
+    50.8997, 4.3389
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "NetHope Summit. San Juan, Puerto Rico. 2019.",
+    18.4632, -66.0858
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "American Association of Geographers Annual Meeting. Washington, DC. 2019.",
+    38.9072, -77.0369
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "French Development Agency. Paris, France. 2019.",
+    48.8566, 2.3522
+  ],
+  [
+    "Green Climate Fund. Incheon, South Korea. 2018.",
+    37.4563, 126.7052
+  ],
+  [
+    "Open Source Center, U.S. Central Intelligence Agency (CIA). Langley, Virginia. 2018.",
+    38.9465, -77.1589
+  ],
+  [
+    "Norwegian Agency for Development Cooperation. Oslo, Norway. 2018.",
+    59.9139, 10.7522
+  ],
+  [
+    "International Civil Society Centre. Berlin, Germany. 2018.",
+    52.5200, 13.4050
+  ],
+  [
+    "Global Environmental Facility. Washington, DC. 2017.",
+    38.9072, -77.0369
+  ],
+  [
+    "World Bank. Washington, DC. 2017.",
+    38.9072, -77.0369
+  ],
+  [
+    "GeoComputation ‘17. Leeds, UK. 2017",
+    53.8008, -1.5491
   ]
 ];
